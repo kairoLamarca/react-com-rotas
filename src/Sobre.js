960712b139d4ddc8cb294b08767e6d2a2e3ec51b
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2018-10-24 17:19:55 -0300

    Initial commit

---
+++ src/Sobre.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Sobre extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Página de Sobre
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Exemplo de Página de Sobre
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Sobre;
