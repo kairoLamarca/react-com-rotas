30ceea49f3a28d6f6ef3ba93f863a43ab72ff0bb
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-10-24 20:51:46 -0300

    get por fetch e por axios
    get por fetch e por axios

--- src/Sobre.js
+++ src/Sobre.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Menu from './Menu';
+import axios from 'axios';
 
 class Sobre extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { teste: '1234', value: 'hhhh' };
+    this.state = { teste: '1234', value: 'hhhh', resultado: [] };
 
     this.handleChange = this.handleChange.bind(this);
+    this.getTesteFetch = this.getTesteFetch.bind(this);
   }
 
   handleChange(event) {
@@ -17,12 +19,36 @@ class Sobre extends Component {
     this.setState({ value: event.target.value });
   }
 
+  getTesteFetch() {
+    fetch("http://localhost:4000/mysql")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
+
+  getTesteAxios = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/mysql');
+      console.log(response.data);
+      this.setState({resultado: response.data});
+
+      console.log('state: ', this.state.resultado);
+    } catch (error) {
+      //console.error(error);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Menu />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Página de Sobre
           </p>
@@ -34,10 +60,16 @@ class Sobre extends Component {
           >
             Exemplo de Página de Sobre {this.state.teste}
           </a>
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-            <p>
-              Value: {this.state.value}
-            </p>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <p>
+            Value: {this.state.value}
+          </p>
+
+          <button onClick={this.getTesteFetch}>Buscar API Fetch</button>
+          <button onClick={this.getTesteAxios}>Buscar API Axios</button>
+          <p>
+            {/* Resultado: {this.state.resultado[0].nome} */}
+          </p>
         </header>
       </div>
     );
