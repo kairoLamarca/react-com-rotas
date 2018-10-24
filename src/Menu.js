57f2c42ee0491c76ef89312586b573f7e3b8af64
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-10-24 19:25:04 -0300

    exemplo de style
    exemplo de style

--- src/Menu.js
+++ src/Menu.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Link } from 'react-router-dom'
+import './Style.css';
+import { Link,  } from 'react-router-dom'
 
 class Menu extends Component {
   render() {
     return (
-      <div className="App">               
+      <div className="menu">               
           <Link to="/">Ir para a página inicial</Link>
           <Link to="/sobre">Ir para a página sobre</Link>
       </div>
