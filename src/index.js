032d9e498e08e76a3b7d3d50c1c52cfa4e149713
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2018-10-24 17:57:02 -0300

    roteamento com react-router-dom
    roteamento com react-router-dom. Roteando o index e o sobre

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Sobre from './Sobre';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom' // importando o BrowserRouter do pacote que acabamos de instalar
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            <Route path="/sobre" exact={true} component={Sobre} />
+        </Switch>
+    </BrowserRouter>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
